d635fbfda2995593b82561e3b7e5df92e18979a9
DemProgr <116211245+DemProgr@users.noreply.github.com>
2024-02-06 23:49:29 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: " акое свойство запрещ€ет выделение текста?",
+        answer: "user-select: none",
+        options: [
+            "no-select: none",
+            "pointer-select: none",
+            "user-select: unset",
+            "user-select: none"
+        ]
+    },
+    {
+        question: " акой атрибут HTML5 указывает, что элемент €вл€етс€ недоступным дл€ редактировани€?",
+        answer: "readonly",
+        options: [
+            "disabled",
+            "readonly",
+            "required",
+            "tabindex"
+        ]
+    },
+    {
+        question: " акой из следующих атрибутов не €вл€етс€ новым в HTML5?",
+        answer: "checked",
+        options: [
+            "autofocus",
+            "placeholder",
+            "required",
+            "checked"
+        ]
+    },
+    {
+        question: "«начением атрибута border может быть:",
+        answer: "2",
+        options: [
+            "top",
+            "left",
+            "2",
+            "green"
+        ]
+    },
+    {
+        question: "ƒл€ чего нужен атрибут autocomplete='off'?",
+        answer: "откл автозаполнени€",
+        options: [
+            "откл автозаполнени€",
+            "вкл автозаполнени€",
+            "делает input об€зательным дл€ заполнени€",
+            "скрывает input"
+        ]
+    },
+]
